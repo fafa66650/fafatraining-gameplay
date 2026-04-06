9f6c77708f4ffd15c2d988ec991cb265358f2076
fafa66650 <doudoumetis@yahoo.fr>
2026-04-06 11:38:28 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+FAFATRAINING MINDGAME V4 ROYALE
+
+Contenu :
+- 108 scénarios
+- 6 familles de jeux
+- 3 catégories d’âge
+- 4 niveaux de difficulté
+- console animateur
+- événements dynamiques automatiques
+- minute par minute
+- cartes rôles imprimables
+- fiches équipes ultra visuelles
+- 48 QR / codes / niveaux
+- 10 packs univers prêts terrain
+- export imprimable / PDF
+- PWA / hors ligne
---
+++ service-worker.js
@@ -0,0 +1,10 @@
+
+const CACHE='fafa-mindgame-v4';
+const ASSETS=['./','./index.html','./style.css','./app.js','./data.js','./scenarios.json','./codes.json','./packs.json','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./assets/logo-fafatraining.jpeg'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    const copy = res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
