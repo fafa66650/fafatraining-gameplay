f7ee3e6baaa7d74e095c0ba6165ced398dee9ffe
fafa <doudoumetis@yahoo.fr>
2026-04-10 23:01:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'fafa-god-mode-final-github-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './sw.js',
+  './assets/logo.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
